e7a29b6292676a360c5fe73777d2c6807e583cf5
Mike-D22 <qamp26@gmail.com>
2023-11-02 14:52:15 -0700

    Updates

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,57 +1,111 @@
 #include"binary_trees.h"
 /**
- * binary_tree_levelorder - IT Traverst binary tree using level-order traverse
- * @tree: tree to traverse.
- * @func: pointer to a func to call for each node
+ * struct node_s - Singly linked list.
+ * @node: const binary tree node,
+ * @next: points to the next node
+ */
+
+typedef struct node_s
+{
+	const binary_tree_t *node;
+	struct node_s *next;
+} ll;
+
+ll *append(ll *head, const binary_tree_t *btnode);
+void free_list(ll *head);
+ll *get_children(ll *head, const binary_tree_t *parent);
+void levels_rec(ll *head, void (*func)(int));
+/**
+ * binary_tree_levelorder - Goes thru binary tree
+ *                          using level-order traversal.
+ * @tree: Pointer to the root node of the tree to traverse.
+ * @func: Pointer to a function to call for each node.
  */
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	size_t level, maxlevel;
+	ll *children = NULL;
 
-	if (!tree || !func)
+	func(tree->n);
+	children = get_children(children, tree);
+	levels_rec(children, func);
+
+	free_list(children);
+}
+/**
+ * levels_rec - Calls funct on all nodes at each level,
+ * @head: Pointer to head of linked list with nodes at one level.
+ * @func: Pointer to a function to call for each node.
+ */
+void levels_rec(ll *head, void (*func)(int))
+{
+	ll *children = NULL, *curr = NULL;
+
+	if (!head)
 		return;
-
-	maxlevel = binary_tree_height(tree) + 1;
-
-	for (level = 1; level <= maxlevel; level++)
-		btlo_helper(tree, func, level);
-}
-
-/**
- * btlo_helper - IT goes thru a binary tree using post-order traverse.
- * @tree: tree to traverse.
- * @func: pointer to a function to call for each node
- * @level: the level of the tree to call func upon
- */
-
-void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level)
-{
-	if (level == 1)
-		func(tree->n);
-	else
+	for (curr = head; curr != NULL; curr = curr->next)
 	{
-		btlo_helper(tree->left, func, level - 1);
-		btlo_helper(tree->right, func, level - 1);
+		func(curr->node->n);
+		children = get_children(children, curr->node);
 	}
+	levels_rec(children, func);
+	free_list(children);
+}
+/**
+ * get_children - Appends children of parent to linked list,
+ * @head: Pointer to head of linked list where children will be appended,
+ * @parent: Pointer to node whose children we want to append.
+ * Return: Pointer to head of linked list of children.
+ */
+ll *get_children(ll *head, const binary_tree_t *parent)
+{
+	if (parent->left)
+		head = append(head, parent->left);
+	if (parent->right)
+		head = append(head, parent->right);
+	return (head);
+}
+/**
+ * append - Addzs new node at the end of linkedlist.
+ * @head: pointer to head of linked list,
+ * @btnode: const binary tree node to append.
+ * Return: A pointer to head, or NULL on failure
+ */
+
+ll *append(ll *head, const binary_tree_t *btnode)
+{
+	ll *new = NULL, *last = NULL;
+
+	new = malloc(sizeof(*new));
+	if (new)
+	{
+		new->node = btnode;
+		new->next = NULL;
+		if (!head)
+			head = new;
+		else
+		{
+			last = head;
+			while (last->next)
+				last = last->next;
+			last->next = new;
+		}
+	}
+	return (head);
 }
 
 /**
- * binary_tree_height -it measures the height of a binary tree.
- * @tree: tree to measure the height of
- *
- * Return: height of the tree
- *         0 if tree is NULL
+ * free_list - IT Frees all nodes in linked list,
+ * @head: pointer to the head of list_t linked list.
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+void free_list(ll *head)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	ll *ptr = NULL;
 
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	while (head)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
 }
--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,14 +1,15 @@
-#include "binary_trees.h"
-#include "limits.h"
+#include"binary_trees.h"
+#include"limits.h"
 
 /**
- * is_bst_helper - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
+ * is_bst_helper -IT Checks if binary tree is valid binary search tree
+ * @tree: A pointer to the root node of the tree to check,
  * @lo: The value of the smallest node visited thus far.
  * @hi: The value of the largest node visited this far.
  *
  * Return: If the tree is a valid BST, 1, otherwise, 0.
  */
+
 int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 {
 	if (tree != NULL)
@@ -22,11 +23,12 @@ int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
 }
 
 /**
- * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
+ * binary_tree_is_bst -It Checkss if binary tree is valid binary search tree
  * @tree: A pointer to the root node of the tree to check.
  *
  * Return: 1 if tree is a valid BST, and 0 otherwise
  */
+
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
