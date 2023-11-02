baed3698ec4ea5a21e73766edc85e0840274ff8f
Mike-D22 <qamp26@gmail.com>
2023-11-01 17:50:18 -0700

    Update now

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,6 +1,49 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_complete -IT checks if the binary tree is complete.
+ * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid Max Binary Heap
+ *         0 if tree is NULL
+ *         0 otherwise
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (btih_helper(tree));
+}
+
+/**
+ * btih_helper - checks if a binary tree is a valid Max Binary Heap
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid Max Binary Heap
+ *         1 if tree is NULL
+ *         0 otherwise
+ */
+int btih_helper(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+
+	if (!binary_tree_is_complete(tree))
+		return (0);
+
+	if (tree->left)
+		if (tree->left->n > tree->n)
+			return (0);
+	if (tree->right)
+		if (tree->right->n > tree->n)
+			return (0);
+
+	return (btih_helper(tree->left) &&
+		btih_helper(tree->right));
+}
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete
  * @tree: a pointer to the root node of the tree to check
  *
  * Return: 1 if the tree is complete
@@ -19,8 +62,8 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 }
 
 /**
- * btic_helper - IT checks if the binary tree is complete
- * @tree: pointer to the root node of the tree to check
+ * btic_helper - checks if a binary tree is complete
+ * @tree: a pointer to the root node of the tree to check
  * @index: node index to check
  * @size: number of nodes in the tree
  *
